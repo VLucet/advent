9f72f54613a8b2ace1f4337dccde88f73eec9564
vlucet <valentin.lucet@gmail.com>
2023-12-09 11:41:00 -0500

    short and sweer

---
+++ puzzles/day_9.R
@@ -0,0 +1,64 @@
+library(advent)
+
+input <- get_puzzle_input(2023,9)
+
+test_input <- c(
+"0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+"
+) |> strsplit("\n") |> unlist()
+
+
+# -------------------------------------------------------------------------
+
+# Part 1
+
+# input <- test_input
+
+parsed <-
+  sapply(input, simplify = F, USE.NAMES = F,
+         \(x) sapply(unlist(strsplit(x, " ")), as.numeric, simplify = T, USE.NAMES = F))
+head(parsed)
+
+derive_to_0 <- function(x) {
+  deriv_list <- list()
+  while (!all(x == 0)) {
+    x <- diff(x)
+    deriv_list <- append(deriv_list, list(x))
+  }
+  return(deriv_list)
+}
+
+parsed_derived <- sapply(parsed, derive_to_0)
+head(parsed_derived)
+
+mapply(parsed_derived, parsed,
+      FUN = \(x, y) rev(y)[1] + rev(cumsum(sapply(x, \(x) rev(x)[1])))[1]) |>
+  sum()
+
+# Part 2
+
+# input <- test_input
+
+parsed <-
+  sapply(input, simplify = F, USE.NAMES = F,
+         \(x) sapply(unlist(strsplit(x, " ")), as.numeric, simplify = T, USE.NAMES = F))
+head(parsed)
+
+derive_to_0 <- function(x) {
+  deriv_list <- list()
+  while (!all(x == 0)) {
+    x <- diff(x)
+    deriv_list <- append(deriv_list, list(x))
+  }
+  return(deriv_list)
+}
+
+parsed_derived <- sapply(parsed, derive_to_0)
+head(parsed_derived)
+
+mapply(parsed_derived, parsed,
+       FUN = \(x, y) rev(y)[1] + rev(cumsum(sapply(x, \(x) rev(x)[1])))[1]) |>
+  sum()
+
